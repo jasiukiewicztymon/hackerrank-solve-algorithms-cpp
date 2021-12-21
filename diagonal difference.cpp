b86b39a1899b6365d96a6629fd70683f6db3bf36
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2021-12-21 22:25:24 +0100

    Create diagonal difference.cpp

---
+++ diagonal difference.cpp
@@ -0,0 +1,28 @@
+
+using namespace std;
+int main() 
+{
+  int N, Sum1 = 0, Sum2 = 0;
+  cin >> N;
+  int A[N][N];
+  for (int i = 0; i < N; i++) 
+  {
+    for (int j = 0; j < N; j++) 
+    {
+      cin >> A[i][j];
+      if (i == j) 
+        Sum1 += A[i][j];
+    }
+  }
+  
+  for (int i = 0; i < N; i++) 
+  {
+    for (int j = N - 1 - i; j >= 0;) 
+    {
+      Sum2 += A[i][j];
+      break;
+    }
+  }
+  cout << abs(Sum1 - Sum2) << endl;
+  return 0;
+}
