c4818d5c223fde1c801bdd6cfce4734f5b398669
Jasiukiewicz Tymon <JasiukiewiczTymon@outlook.fr>
2023-04-16 20:42:28 +0200

    Create 2D Array - DS.cpp

---
+++ 2D Array - DS.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+int hourglassSum(vector<vector<int>> arr) {
+    int max = arr[0][0] + arr[0][1] + arr[0][2] + arr[1][1] + arr[2][0] + arr[2][1] + arr[2][2];
+    for (int i = 0; i < arr.size() - 2; i++) {
+        int s = arr[i][0] + arr[i][1] + arr[i][2] + arr[i + 1][1] + arr[i + 2][0] + arr[i + 2][1] + arr[i + 2][2];
+        
+        if (max < s)
+            max = s;
+        
+        for (int j = 1; j < arr[i].size() - 2; j++) {
+            s = s - (arr[i][j - 1] + arr[i + 1][j] + arr[i + 2][j - 1]) + (arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j + 2]); 
+            
+            if (max < s)
+                max = s; 
+        }
+    }
+    
+    return max;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    vector<vector<int>> arr(6);
+
+    for (int i = 0; i < 6; i++) {
+        arr[i].resize(6);
+
+        string arr_row_temp_temp;
+        getline(cin, arr_row_temp_temp);
+
+        vector<string> arr_row_temp = split(rtrim(arr_row_temp_temp));
+
+        for (int j = 0; j < 6; j++) {
+            int arr_row_item = stoi(arr_row_temp[j]);
+
+            arr[i][j] = arr_row_item;
+        }
+    }
+
+    int result = hourglassSum(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
